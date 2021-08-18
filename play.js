c567479e61531a34c86abfdd010520ea72982364
Noordeep Punjabi <punjabi.noordeep@gmail.com>
2021-08-17 22:24:38 -0400

    implement callback function for stdin data to allow for program to exit if ctrl+c is pressed

--- play.js
+++ play.js
@@ -1,3 +1,24 @@
+/* eslint-disable func-style */
 const connect = require('./client');
 console.log("Connecting ...");
 connect();
+
+// following code sets up an user interface to take in user inputs using stdin
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  function handleUserInput(key) {
+    if (key === '\u0003') {
+      process.exit();
+    }
+  }
+  stdin.on('data', key => {
+    handleUserInput(key);
+  });
+  return stdin;
+};
+
+setupInput();
