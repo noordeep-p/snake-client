8ca8e1af86cb5950c73729c0cc158e8ee367c2b1
Noordeep Punjabi <punjabi.noordeep@gmail.com>
2021-08-18 19:55:13 -0400

    update cleint to take local server data and push it to remote snake server

--- client.js
+++ client.js
@@ -13,7 +13,7 @@ const connect = function() {
   conn.setEncoding("utf8");
 
   conn.on("data", (data) => {
-    console.log("Server says:", data);
+    conn.write(data);
   });
 
   conn.on("connect", () => {
