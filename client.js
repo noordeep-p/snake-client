60dda57bd5305cd5c80dfe4bf37df88ca6121957
Noordeep Punjabi <punjabi.noordeep@gmail.com>
2021-08-18 21:03:45 -0400

    DRY up code

--- client.js
+++ client.js
@@ -12,28 +12,16 @@ const connect = function() {
   // interpret the data incoming as text
 
   conn.setEncoding("utf8");
-
+  
   conn.on("data", (data) => {
     console.log(`Server says: ${data}`);
   });
-
-  conn.on("data", (data) => {
-    conn.write(data);
-  });
-
+  
   conn.on("connect", () => {
     console.log("Connection with server established!");
-  });
-
-  conn.on("connect", () => {
     conn.write("Name: NSP");
   });
-
-  // folowing code is used as an example exercise to test sanke moves but is hard coded and thus commented out
-  // conn.on("connect", () => {
-  //   conn.write("Move: up");
-  // });
-  
+ 
   return conn;
 };
 
